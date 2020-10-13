bd32a89f9cc2a757bfade64ea790e2545b63be56
parkgeonwoong <65653053+parkgeonwoong@users.noreply.github.com>
2020-10-14 00:37:54 +0900

    Shape.java
    Shape

--- Shape.java
+++ Shape.java
@@ -1,23 +1,29 @@
 package hello;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 public abstract class Shape {
-	int x, y, w, h;
-	boolean Shape_object = true;	//true = circle , false = rect 
+	public int x, y, w, h;
+	public int Shape_object;	//1 = rect  , 2 = circle 
+	public boolean fill;
+	public Color color;
 	
-	public Shape(int x, int y, int w, int h, boolean Shape_object) {
+	
+	public Shape(int x, int y, int w, int h, int Shape_object, boolean fill, Color color) {
 		this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = h;
 		this.Shape_object = Shape_object;
+		this.fill = fill;
+		this.color = color;
 	}
 	
 	
 	public abstract void paint(Graphics g);
 	
-//	ë„í˜• ì´ë™ì‹œ ë„í˜•ì„ í´ë¦­ í•˜ì˜€ì„ë•Œ Check_in, Check_out í™•ì¸
+//	µµÇü ÀÌµ¿½Ã µµÇüÀ» Å¬¸¯ ÇÏ¿´À»¶§ Check_in, Check_out È®ÀÎ
 	public boolean isIn(int tx, int ty) { 	
 		if(x < tx && tx < x+w) {
 			if(y < ty && ty < y+h) {
