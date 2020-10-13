15386bf8303b0f491bc6f144cd4fe7dc0e61ea41
parkgeonwoong <65653053+parkgeonwoong@users.noreply.github.com>
2020-10-14 00:38:02 +0900

    Circle.java

--- Circle.java
+++ Circle.java
@@ -1,15 +1,22 @@
 package hello;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class Circle extends Shape{
 
-	public Circle(int x, int y, int w, int h, boolean Shape_object) {
-		super(x, y, w, h, Shape_object);
+	public Circle(int x, int y, int w, int h, int Shape_object, boolean fill, Color color) {
+		super(x, y, w, h, Shape_object, fill, color);
 	}
 	
 	public void paint(Graphics g) {
-		g.drawOval(x, y, w, h);
+		if(fill == true) {
+			g.setColor(color);
+			g.fillOval(x, y, w, h);
+		} else if (fill == false) {
+			g.setColor(color);
+			g.drawOval(x, y, w, h);
+		}
 	}
 	
 }
